9ad80ccd8bce4e83410bb45f7d17e7b9068cfe38
Lorena <lorena.giddens@outlook.com>
2018-06-16 02:34:24 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,15 +1,8 @@
 class Post
   attr_accessor :title, :author
 
-  @@all = []
-
   def initialize(title)
     @title = title
-    @@all << self
-  end
-
-  def self.all
-    @@all
   end
 
   def author_name
