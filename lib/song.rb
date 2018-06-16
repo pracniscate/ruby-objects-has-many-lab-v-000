58deddc0de0a49d739c36a7530c883a16c5a70da
Lorena <lorena.giddens@outlook.com>
2018-06-16 02:35:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,19 +1,13 @@
 class Song
   attr_accessor :artist, :name
 
-  @@all = []
-
   def initialize(name)
     @name = name
-    @@all << self
   end
 
-  def self.all
-    @@all
-  end
 
   def artist_name
-    self.artist ? self.artist.name : nil
+    self.artist ? self.artist_name : nil
   end
 
   def artist_name
