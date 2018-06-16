f3e1a746889ef956b7d93d09b6dfb2d88b1ad189
Lorena <lorena.giddens@outlook.com>
2018-06-16 02:21:52 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -18,11 +18,8 @@ class Author
 
   # create a new post with the name argument and associate the post and the author
   def add_post_by_title(title)
-    post = Post.new(title)
-    @posts << post
-    post.author = self # object reciprocity
-    # return the post
-    post
+    new_post = Post.new(title)
+    self.add_post(new_post)
   end
 
   def self.post_count
